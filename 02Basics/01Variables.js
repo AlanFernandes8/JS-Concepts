0e59e619c80d5a445951ac81f27da34de953f16b
Alan Fernandes <alanferns19@gmail.com>
2024-09-13 20:06:18 +0530

    ok

---
+++ 02Basics/01Variables.js
@@ -0,0 +1,18 @@
+var name = "Alan";
+
+var course = "React JS";
+
+var isLoggedIn = true;
+
+var loggedCount = 23;
+
+// console.log(loggedCount);
+
+var paymentMode;
+
+
+
+
+paymentMode = "Debit Card";
+
+console.log(paymentMode);
